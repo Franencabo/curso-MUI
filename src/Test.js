dc73b91451f0810ce9e097a873f7c418e71f4067
Fran <franencabo@gmail.com>
2024-07-03 22:29:39 +0200

    TextFields into a form

--- src/Test.js
+++ src/Test.js
@@ -1,32 +1,65 @@
 import React from "react";
 
-import { TextField, Typography } from "@mui/material";
+import { Button, TextField, Typography } from "@mui/material";
 
 const Test = () => {
-  const [value, setValue] = React.useState("");
+  const [inputs, setInputs] = React.useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    return inputs;
+  };
+
   return (
     <div>
-      <TextField
-        type="text"
-        sx={{ margin: 3 }}
-        placeholder="Name"
-        onChange={(e) => setValue(e.target.value)}
-        variant="outlined"
-      />
-      <TextField
-        type="email"
-        sx={{ margin: 3 }}
-        placeholder="Email"
-        variant="standard"
-      />
-      <TextField
-        type="password"
-        sx={{ margin: 3 }}
-        placeholder="Passowrd"
-        variant="filled"
-      />
-      <Typography sx={{ margin: 3 }} variant="h1">
-        {value}
+      <form>
+        <TextField
+          name="name"
+          onChange={handleChange}
+          value={inputs.name}
+          type="text"
+          sx={{ margin: 3 }}
+          placeholder="Name"
+          variant="outlined"
+        />
+        <TextField
+          name="email"
+          onChange={handleChange}
+          value={inputs.email}
+          type="email"
+          sx={{ margin: 3 }}
+          placeholder="Email"
+          variant="standard"
+        />
+        <TextField
+          name="password"
+          onChange={handleChange}
+          value={inputs.password}
+          type="password"
+          sx={{ margin: 3 }}
+          placeholder="Passowrd"
+          variant="filled"
+        />
+
+        <Button
+          onSubmit={handleSubmit}
+          sx={{ margin: 3 }}
+          type="submit"
+          variant="contained"
+        >
+          Submit
+        </Button>
+      </form>
+      <Typography sx={{ margin: 3 }} variant="h5">
+        {JSON.stringify(inputs)}
       </Typography>
     </div>
   );
